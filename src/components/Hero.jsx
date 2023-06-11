9334ed801041ba7748795ca124d41b807008ea5c
Hatish <himanshuasija786@gmail.com>
2023-06-11 15:47:22 +0530

    Navbar and heading updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,15 @@ const Hero = () => {
         <div className='w-5 h-5 rounded-full bg-[#915eff]' />
         <div className='w-1 sm:h-80 h-40 violet-gradient'/>
       </div>
+
+      <div>
+        <h1 className={`${styles.heroHeadText} text-white`}>Hi, I am <span className='text-[#915eff]'>Hatish</span></h1>
+        <p className= {`${styles.heroSubText} mt-2 text-white-100`}>
+          I build things for the web.
+        </p>
+      </div>
     </div>
+      <ComputersCanvas />
     </section>
   )
 }
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import {Suspense, useEffect, useState} from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+
 
 const Computers = () => {
   return (
